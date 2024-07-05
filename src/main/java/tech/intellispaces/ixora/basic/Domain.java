96175b4ea7a8fc7049672619f8057fa6dfdec2c9
lavor <lavor.andrey@gmail.com>
2024-07-05 09:35:42 +0600

    Transition domain added

--- src/main/java/tech/intellispaces/ixora/basic/Domain.java
+++ src/main/java/tech/intellispaces/ixora/basic/Domain.java
@@ -1,5 +1,13 @@
 package tech.intellispaces.ixora.basic;
 
+/**
+ * Space domain.<p/>
+ *
+ * Domain types can include type parameters. Type parameters indicates Java types associated
+ * with a specific object of the given domain.
+ *
+ * @param <D> Java type associated with domain.
+ */
 @tech.intellispaces.framework.core.annotation.Domain("1a243181-9d2b-48d2-bbcf-cd1f18311011")
 public interface Domain<D> {
 }
