c03136c9b82e38ae06b0b16a3f46f9f63cf0deac
lavor <lavor.andrey@gmail.com>
2024-07-30 17:13:14 +0600

    Rename module and packages

--- src/main/java/intellispaces/ixora/core/Domain.java
+++ src/main/java/intellispaces/ixora/core/Domain.java
@@ -1,15 +1,15 @@
-package intellispaces.ixora.mindstructs.core;
+package intellispaces.ixora.core;
 
-import tech.intellispaces.framework.core.annotation.Transition;
+import tech.intellispaces.core.annotation.Transition;
 
 /**
- * Space domain.<p/>
+ * Semantic space domain.<p/>
  *
  * Each point of this domain represents domain.<p/>
  *
  * Any domain type can include type parameters. Type parameters indicates connection of this domain with other domains.
  */
-@tech.intellispaces.framework.core.annotation.Domain("1a243181-9d2b-48d2-bbcf-cd1f18311011")
+@tech.intellispaces.core.annotation.Domain("1a243181-9d2b-48d2-bbcf-cd1f18311011")
 public interface Domain extends Point {
 
   @Override
