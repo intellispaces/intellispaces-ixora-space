fd70b8df9eb8bd93840ee93e4a1074b7b9a4c175
lavor <lavor.andrey@gmail.com>
2024-08-23 11:07:07 +0600

    Rename domain classes in according to name convention

--- ixora-basic-ontology/src/main/java/intellispaces/ixora/basic/DomainDomain.java
+++ ixora-basic-ontology/src/main/java/intellispaces/ixora/basic/DomainDomain.java
@@ -1,17 +1,16 @@
 package intellispaces.ixora.basic;
 
+import intellispaces.core.annotation.Domain;
 import intellispaces.core.annotation.Transition;
 
 /**
- * Semantic space domain.<p/>
- *
- * Each point of this domain represents domain.<p/>
+ * Domain of objects represented semantic space domains.<p/>
  *
  * Any domain type can include type parameters. Type parameters indicates connection of this domain with other domains.
  */
-@intellispaces.core.annotation.Domain("1a243181-9d2b-48d2-bbcf-cd1f18311011")
-public interface Domain extends Point {
+@Domain("1a243181-9d2b-48d2-bbcf-cd1f18311011")
+public interface DomainDomain extends PointDomain {
 
   @Transition("1d9a3eaf-cb88-4d0a-84a1-5e6b21235aa9")
-  Point asPoint();
+  PointDomain asPoint();
 }
--- ixora-basic-ontology/src/main/java/intellispaces/ixora/basic/PointDomain.java
+++ ixora-basic-ontology/src/main/java/intellispaces/ixora/basic/PointDomain.java
@@ -5,11 +5,14 @@ import intellispaces.core.annotation.Transition;
 import intellispaces.javastatements.type.Type;
 
 /**
- * Semantic space point.
+ * Domain of objects represented semantic space points.
  */
 @Domain("01909034-27be-70d5-8033-7967d56fd62d")
-public interface Point {
+public interface PointDomain {
 
+  /**
+   * Basic point domain.
+   */
   @Transition("bcdfe93c-e2ec-4c51-a83b-e2ff2b48b9db")
   Type<?> domain();
 }
